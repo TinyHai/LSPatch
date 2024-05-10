e39502fc2aca12487342d35d80f4eb06bfcb10c8
TinyHai <1035908357@qq.com>
2024-05-10 19:16:27 +0800

    remove InitSymbolCache

--- patch-loader/src/main/jni/src/patch_loader.cpp
+++ patch-loader/src/main/jni/src/patch_loader.cpp
@@ -84,7 +84,6 @@ namespace lspd {
     }
 
     void PatchLoader::Load(JNIEnv *env) {
-        InitSymbolCache(nullptr);
         lsplant::InitInfo initInfo{
                 .inline_hooker = [](auto t, auto r) {
                     void *bk = nullptr;
